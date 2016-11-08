3e231bb7930759ae43ca6c00d1773e7620db0a7e
毛才明 <maocaiming@ucredit.com>
2016-11-08 16:37:45 +0800

    Exception

---
+++ ExceptionTest/ExceptionTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using Exceptionless;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exceptionless.Configuration;
+using System.IO;
+using log4net.Config;
+using log4net;
+using StackExchange.Redis;
+//[assembly: Exceptionless("oXX5BJqhS30ni045BqthqJtiSnpB0naMactfmYmI", ServerUrl = "http://localhost:8004")]
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo fi = new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log4net.config"));
+            XmlConfigurator.ConfigureAndWatch(fi);
+            ILog logger= LogManager.GetLogger(typeof(Program));
+            ExceptionlessClient.Default.Startup("oXX5BJqhS30ni045BqthqJtiSnpB0naMactfmYmI");
+            //var client = new ExceptionlessClient(c => {
+            //    c.ApiKey = "oXX5BJqhS30ni045BqthqJtiSnpB0naMactfmYmI";
+            //    c.ServerUrl = "http://localhost:8004";
+            //});
+            //throw new Exception("test exception " + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"));
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+            string value = "aaa";
+            db.StringSet("mykey", value);
+            string value2 = db.StringGet("mykey");
+            Console.WriteLine(value2); 
+            try
+            {
+                //throw new Exception("test exception "+DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"));
+            }
+            catch (Exception ex)
+            {
+                //client.SubmitException(ex);
+                //ex.ToExceptionless().Submit();
+                //logger.Error(ex);
+                //Console.WriteLine("error send");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExceptionTest/packages/Exceptionless.Log4net.4.0.1902/readme.txt
@@ -0,0 +1,34 @@
+﻿-------------------------------------
+		Exceptionless Readme
+-------------------------------------
+Exceptionless provides real-time error reporting for your apps. It organizes the 
+gathered information into simple actionable data that will help your app become 
+exceptionless!
+
+Learn more at http://exceptionless.io.
+
+-------------------------------------
+		How to get an api key
+-------------------------------------
+The Exceptionless client requires an api key to use the Exceptionless service. 
+You can get your Exceptionless api key by logging into http://exceptionless.io 
+and viewing your project configuration page.
+
+-------------------------------------
+		 log4net Integration
+-------------------------------------
+
+Here is an example log.config file that shows how to use the Exceptionless log4net appender.
+
+<appender name="exceptionless" type="Exceptionless.Log4net.ExceptionlessAppender,Exceptionless.Log4net" />
+
+By default, the appender will use the settings from the Exceptionless config section, but
+you can also set it on the appender like this:
+
+<appender name="exceptionless" type="Exceptionless.Log4net.ExceptionlessAppender, Exceptionless.Log4net">
+    <apiKey value="API_KEY_HERE" />
+</appender>
+-------------------------------------
+	  Documentation and Support
+-------------------------------------
+Please visit http://exceptionless.io for documentation and support.
